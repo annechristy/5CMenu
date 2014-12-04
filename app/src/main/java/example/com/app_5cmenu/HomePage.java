300665fb2e11520074f02d268a7827e2ca5d406c
Anne Christy <achristy@hmc.edu>
2014-12-04 15:39:30 -0800

    polished off app style

--- app/src/main/java/example/com/app_5cmenu/HomePage.java
+++ app/src/main/java/example/com/app_5cmenu/HomePage.java
@@ -90,11 +90,12 @@ public class HomePage extends Activity implements View.OnClickListener {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+       /* int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
         }
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item);*/
+        return false;
     }
 
     @Override
--- app/src/main/java/example/com/app_5cmenu/MenuPage.java
+++ app/src/main/java/example/com/app_5cmenu/MenuPage.java
@@ -273,8 +273,8 @@ public class MenuPage extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_page, menu);
-        return true;
+        //getMenuInflater().inflate(R.menu.menu_page, menu);
+        return false;
     }
 
     @Override
@@ -282,11 +282,12 @@ public class MenuPage extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+       /* int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
         }
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item);*/
+        return false;
     }
 
 }
